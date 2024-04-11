333ba3916f151b4ad451744ad65cb81ed8d7bc2f
itm-platform <48089993+itm-platform@users.noreply.github.com>
2024-04-11 19:17:25 +0200

    changed data files location

--- tests/effortTransformer.spec.js
+++ tests/effortTransformer.spec.js
@@ -1,7 +1,9 @@
 import { describe, test, expect } from 'vitest';
 import { EffortTransformer } from '../effortTransformer';
-import responseResourceAnalysis from '../responseResourceAnalysis.js';
-import intervalsByEntity from '../dataSamples/intervalsByEntity.js';
+import responseResourceAnalysis from './dataSamples/responseResourceAnalysis.js';
+import intervalsByEntity from './dataSamples/intervalsByEntity.js';
+import intervalsByUser from './dataSamples/intervalsByUser.js';
+
 // import fs
 import fs from 'fs';
 describe('effortTransformer', () => {
@@ -11,6 +13,13 @@ describe('effortTransformer', () => {
         // write the result in a file
         fs.writeFileSync('intervalsByEntityTestResult.json', JSON.stringify(result, null, 2));
         expect(result).toEqual(intervalsByEntity);
+    });
+
+    test('transformToIntervalsByUser', () => {
+        const result = effortTransformer.transformToIntervalsByUser();
+        // write the result in a file
+        fs.writeFileSync('intervalsByUserTestResult.json', JSON.stringify(result, null, 2));
+        expect(result).toEqual(intervalsByUser);
     });
 
 });
