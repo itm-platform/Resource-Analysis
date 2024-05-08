bb9aa91165a807dff442c9a8d894ca6a7307a40e
itm-platform <48089993+itm-platform@users.noreply.github.com>
2024-05-08 14:20:55 +0200

    removed render functions from flexitable

--- flexiTable.js
+++ flexiTable.js
@@ -144,40 +144,6 @@ export class FlexiTable {
         extract(rows);
         return Object.values(userImages); // Convert the object to an array of user image objects
     }
-    renderUserName(params) {
-        const imagePath = pathCache[params.id];
-        if (imagePath) {
-            return `
-            <div class="ftbl-user-wrapper">
-                <img src="${imagePath}" alt="${params.name}" class="ftbl-user-placeholder"/>
-                <span class="ftbl-user-name">${params.name}</span>
-            </div>`;
-        }
-        const initials = params.name.split(' ').slice(0, 2).map(n => n[0]).join('');
-        return `
-        <div class="ftbl-user-wrapper">
-            <div class="ftbl-user-placeholder ftbl-user-initials" title="${params.name}">${initials.toLowerCase()}</div>
-            <span class="ftbl-user-name">${params.name}</span>
-        </div>`;
-    }
-    renderEntityName(params) {
-        const imageNameMap = {
-            'project': {
-                'waterfall': 'Waterfall.svg',
-                'agile': 'Agile.svg'
-            },
-            'service': 'Service.svg'
-        };
-
-        const entityType = params.entityType;
-        const entitySubType = params.entitySubType;
-        const imageName = imageNameMap[entityType]?.[entitySubType] || imageNameMap[entityType];
-        const imagePath = resolveIconPath(imageName);
-        return `<span class="ftbl-entity-icon"><img src="${imagePath}" alt="${entitySubType || entityType}"></span>${params.name}`;
-    }
-    renderDuration(params) {
-        return params.value ? `${params.value / 60} h.` : '';
-    }
     renderHeader() {
         const { headerRow, header } = this.#createHeader();
         this.#addToolbarToHeaderFirstCell(headerRow);
