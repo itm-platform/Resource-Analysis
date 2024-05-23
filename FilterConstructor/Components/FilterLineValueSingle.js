b58757a9bc37c8dd721e6d9ca2b7271574596177
itm-platform <48089993+itm-platform@users.noreply.github.com>
2024-05-23 18:58:03 +0200

    prevent undefined in value

--- FilterConstructor/Components/FilterLineValueSingle.js
+++ FilterConstructor/Components/FilterLineValueSingle.js
@@ -20,13 +20,15 @@ export class FilterLineValueSingle {
 
     /**Create and setup DOM elements*/
     #createElement() {
-        
+
         const component = document.createElement('div');
         component.className = 'filter-line-value';
         this.elements.component = component;
 
         const inputElement = document.createElement('input');
-        inputElement.value = this.value;
+        if (this.value) {
+            inputElement.value = this.value;
+        }
         this.elements.inputElement = inputElement;
 
         component.appendChild(inputElement);
@@ -42,30 +44,30 @@ export class FilterLineValueSingle {
 
     #setupEventListeners() {
         this.elements.inputElement.addEventListener('change', (event) => {
-          let value = event.target.value;
-      
-          if (this.valueType === "Number") {
-            value = Number(value);
-            if (isNaN(value)) {
-              console.warn(`The value ${value} is not a number. Reverting to previous value.`);
-              this.elements.inputElement.value = this.value;
-              return;
+            let value = event.target.value;
+
+            if (this.valueType === "Number") {
+                value = Number(value);
+                if (isNaN(value)) {
+                    console.warn(`The value ${value} is not a number. Reverting to previous value.`);
+                    this.elements.inputElement.value = this.value;
+                    return;
+                }
+            } else if (this.valueType === "String") {
+                value = value.toString();
             }
-          } else if (this.valueType === "String") {
-            value = value.toString();
-          }
-      
-          this.value = value;
-      
-          this.elements.component.dispatchEvent(new CustomEvent('filterValueUpdated', {
-            detail: this.value,
-            bubbles: true
-          }));
+
+            this.value = value;
+
+            this.elements.component.dispatchEvent(new CustomEvent('filterValueUpdated', {
+                detail: this.value,
+                bubbles: true
+            }));
         });
-      }
-      
+    }
+
     render() {
-    
+
     }
 
 
